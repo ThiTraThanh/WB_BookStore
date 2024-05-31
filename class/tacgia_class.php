b160c198d36d289914b1ca56c18b8ddd2e74a6f6
TranDinhThuyTien <trandinhthuytien2004@gmail.com>
2024-06-01 00:54:45 +0700

    Create tacgia_class.php

---
+++ class/tacgia_class.php
@@ -0,0 +1,41 @@
+<?php
+    include "database.php";
+?>
+<?php
+class TacGia {
+    private $db;
+    public function __construct()
+    {
+        $this -> db = new Database();
+    }
+    public function insert_TacGia($tenTacGia){
+        $query="INSERT INTO  tacgia(tenTacGia) VALUES ('$tenTacGia') ";
+        $result = $this -> db -> insert($query);
+        header('Location:TacGialist.php');
+        return $result;
+    }
+    public function show_TacGia(){
+        $query = "SELECT * FROM tacgia ORDER BY maTacGia DESC";
+        $result = $this -> db -> select($query);
+        return $result;
+    }
+    public function get_TacGia($maTacGia) {
+        $query = "SELECT * FROM tacgia where maTacGia='$maTacGia' ";
+        $result = $this -> db -> select($query);
+        return $result;
+    }
+    public function update_TacGia($tenTacGia,$maTacGia) {
+        $query = "UPDATE tacgia SET tenTacGia = '$tenTacGia' where maTacGia='$maTacGia' ";
+        $result = $this -> db -> update($query);
+        header('Location:TacGialist.php');
+        return $result;
+    }
+    public function delete_TacGia($maTacGia)
+    {
+        $query= "DELETE FROM tacgia WHERE maTacGia='$maTacGia'";
+        $result = $this -> db -> delete($query);
+        header('Location:TacGialist.php');
+        return $result;
+    }
+}
+?>
