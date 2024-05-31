765b058cded5b0d2f1bc5c8c61c1e85afc4a38af
TranDinhThuyTien <trandinhthuytien2004@gmail.com>
2024-06-01 00:57:15 +0700

    Update product_class.php

--- class/product_class.php
+++ class/product_class.php
@@ -2,138 +2,99 @@
     include "database.php";
 ?>
 <?php
-class product {
+class Sach {
     private $db;
     public function __construct()
     {
         $this -> db = new Database();
     }
     public function show_cartegory(){
-        $query = "SELECT * FROM tbl_cartegory ORDER BY cartegory_id DESC";
+        $query = "SELECT * FROM theloaisach ORDER BY maLoaiSach DESC";
         $result = $this -> db -> select($query);
         return $result;
     }
     public function insert_product(){
-        $product_name = $_POST['product_name'];
-        $cartegory_id= $_POST['cartegory_id'];
-        $brand_id=$_POST['brand_id'];
-        $nxb_name=$_POST['nxb_name'];
-        $tacgia_name=$_POST['tacgia_name'];
-        $product_price=$_POST['product_price'];
-        $product_desc=$_POST['product_desc'];
-        $product_img=$_FILES['product_img']['name'];
-        move_uploaded_file($_FILES['product_img']['tmp_name'],"uploads/".$_FILES['product_img']['name']);
-        $query="INSERT INTO  tbl_product(
-            product_name, 
-            cartegory_id,
-            brand_id,
-            nxb_name,
-            tacgia_name,
-            product_price,
-            product_desc,
-            product_img)
+        $tenSach = $_POST['tenSach'];
+        $maSach = $_POST['maSach'];
+        $maLoaiSach = $_POST['maLoaiSach'];
+        $maNXB= $_POST['maNXB'];
+        $maTacGia=$_POST['maTacGia'];
+        $Gia=$_POST['Gia'];
+        $moTa=$_POST['moTa'];
+        $soLuongConLai=$_POST['soLuongConLai'];
+        $anhBia=$_FILES['anhBia']['name'];
+        $query="INSERT INTO  sach(
+            tenSach, 
+            maSach,
+            maLoaiSach,
+            maNXB,
+            maTacGia,
+            Gia,
+            moTa,
+            soLuongConLai,
+            anhBia)
          VALUES (
-            '$product_name', 
-            '$cartegory_id',
-            '$brand_id',
-            '$nxb_name',
-            '$tacgia_name',
-            '$product_price',
-            '$product_desc',
-            '$product_img'
+            '$tenSach', 
+            '$maSach',
+            '$maLoaiSach',
+            '$maNXB',
+            '$maTacGia',
+            '$Gia',
+            '$soLuongConLai',
+            '$moTa',
+            '$anhBia'
          ) ";
-        $result = $this -> db -> insert($query);
-        if($result){
-            $query = "SELECT * FROM tbl_product ORDER BY product_id DESC LIMIT 1 ";
-            $result= $this -> db -> select($query) -> fetch_assoc();
-            $product_id=$result['product_id'];
-            $filename=$_FILES['product_img_desc']['name'];
-            $filetmp=$_FILES['product_img_desc']['tmp_name'];
-            foreach ($filename as $key => $value)
-            {
-                move_uploaded_file($filetmp [$key],"uploads/".$value);
-                $query ="INSERT INTO tbl_product_img_desc (product_id, product_img_desc) VALUES ('$product_id','$value')";
-                $result = $this -> db -> insert($query);
-            }
-             
-        }
-        //header('Location:brandlist.php');
-        return $result;
+         $result = $this -> db -> insert($query);
+         header('Location:productlist.php');
+         return $result;
+        
     }
     public function show_product(){
         // $query = "SELECT * FROM tbl_brand ORDER BY brand_id DESC";
-        $query= "SELECT tbl_product.*,tbl_brand.brand_name,tbl_cartegory.cartegory_name 
-        FROM tbl_product
-        LEFT  JOIN tbl_brand ON tbl_brand.brand_id=tbl_product.brand_id
-        LEFT JOIN tbl_cartegory ON tbl_cartegory.cartegory_id=tbl_product.cartegory_id
-        ORDER BY tbl_product.product_id DESC";
+        $query= "SELECT sach.*,nxb.tenNXB,theloaisach.tenLoaiSach,tacgia.tenTacGia
+        FROM sach
+         JOIN nxb ON sach.maNXB=nxb.maNXB
+         JOIN theloaisach ON theloaisach.maLoaiSach=sach.maLoaiSach
+         JOIN tacgia ON tacgia.maTacGia=sach.maTacGia
+        ORDER BY sach.maSach DESC";
         $result = $this -> db -> select($query);
         return $result;
     }
-    public function get_product($product_id) {
-        $query = "SELECT * FROM tbl_product where product_id='$product_id' ";
+    public function get_product($maSach) {
+        $query = "SELECT * FROM sach where maSach='$maSach' ";
         $result = $this -> db -> select($query);
         return $result;
     }
-    public function update_product($cartegory_id,$brand_id,$product_name,$product_id){
-        $query = "UPDATE tbl_product SET product_name = '$product_name' ,brand_id='$brand_id',cartegory_id ='$cartegory_id' where product_id='$product_id' ";
+    public function update_product($maNXB,$maTacGia,$tenSach,$maSach){
+        $query = "UPDATE sach SET tenSach = '$tenSach' ,maNXB ='$maNXB', maTacGia='$maTacGia' where maSach='$maSach' ";
         $result = $this -> db -> update($query);
         header('Location:productlist.php');
         return $result;
     }
-    public function insert_brand($cartegory_id,$brand_name){
-        $query="INSERT INTO  tbl_brand(cartegory_id, brand_name) VALUES ('$cartegory_id','$brand_name') ";
-        $result = $this -> db -> insert($query);
-        header('Location:brandlist.php');
-        return $result;
-    }
-    public function show_brand(){
-        // $query = "SELECT * FROM tbl_brand ORDER BY brand_id DESC";
-        $query= "SELECT tbl_brand.*,tbl_cartegory.cartegory_name 
-        FROM tbl_brand INNER JOIN tbl_cartegory ON tbl_brand.cartegory_id=tbl_cartegory.cartegory_id
-        ORDER BY tbl_brand.brand_id DESC";
+    public function show_nxb(){
+        $query = "SELECT * FROM nxb ORDER BY maNXB DESC";
         $result = $this -> db -> select($query);
         return $result;
     }
-    public function get_cartegory($cartegory_id) {
-        $query = "SELECT * FROM tbl_cartegory where cartegory_id='$cartegory_id' ";
+    public function get_nxb($maNXB) {
+        $query = "SELECT * FROM nxb where maNXB='$maNXB' ";
         $result = $this -> db -> select($query);
         return $result;
     }
-    public function get_brand($brand_id) {
-        $query = "SELECT * FROM tbl_brand where brand_id='$brand_id' ";
+    
+    public function show_TacGia(){
+        $query = "SELECT * FROM tacgia ORDER BY maTacGia DESC";
         $result = $this -> db -> select($query);
         return $result;
     }
-    public function update_brand($cartegory_id,$brand_name,$brand_id){
-        $query = "UPDATE tbl_brand SET brand_name = '$brand_name' ,cartegory_id ='$cartegory_id' where brand_id='$brand_id' ";
-        $result = $this -> db -> update($query);
-        header('Location:brandlist.php');
+    public function get_TacGia($maTacGia) {
+        $query = "SELECT * FROM tacgia where maTacGia='$maTacGia' ";
+        $result = $this -> db -> select($query);
         return $result;
     }
-    public function update_cartegory($cartegory_name,$cartegory_id) {
-        $query = "UPDATE tbl_cartegory SET cartegory_name = '$cartegory_name' where cartegory_id='$cartegory_id' ";
-        $result = $this -> db -> update($query);
-        header('Location:cartegorylist.php');
-        return $result;
-    }
-    public function delete_cartegory($cartegory_id)
+    public function delete_product($maSach)
     {
-        $query= "DELETE FROM tbl_cartegory WHERE cartegory_id='$cartegory_id'";
-        $result = $this -> db -> delete($query);
-        header('Location:cartegorylist.php');
-        return $result;
-    }
-    public function delete_brand($brand_id)
-    {
-        $query= "DELETE FROM tbl_brand WHERE brand_id='$brand_id'";
-        $result = $this -> db -> delete($query);
-        header('Location:brandlist.php');
-        return $result;
-    }
-    public function delete_product($product_id)
-    {
-        $query= "DELETE FROM tbl_product WHERE product_id='$product_id'";
+        $query= "DELETE FROM sach WHERE maSach='$maSach'";
         $result = $this -> db -> delete($query);
         header('Location:productlist.php');
         return $result;
